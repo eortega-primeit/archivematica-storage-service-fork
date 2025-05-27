06bd0f128c7ad3490ca9d057d659d82064eef0fa
ext-ernesto.ortega <ext-ernesto.ortega@logalty.com>
2025-05-27 10:39:36 +0200

    fix migracion, dependencia del anterior

--- src/archivematica/storage_service/locations/migrations/0038_Logalty.py
+++ src/archivematica/storage_service/locations/migrations/0038_Logalty.py
@@ -3,6 +3,7 @@ from django.db import models
 
 
 class Migration(migrations.Migration):
+    dependencies = [("locations", "0037_django42")]
     operations = [
         migrations.CreateModel(
             name="Logalty",
